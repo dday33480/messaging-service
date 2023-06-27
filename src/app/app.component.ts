8b6244cb9dbfdff767947224ae7625cb53f0c9d7
dday33480 <daviddufau@hotmail.co.uk>
2023-06-27 20:19:34 +0200

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'messaging-service';
+  title = 'Boost Messenger';
+
+  activeUsers = [
+    {user: 'Bill'},
+    {user: 'Paul'},
+    {user: 'Owen'},
+  ];
+
+  messageDate = new Date();
+
+  userMessages = [
+    {message:"Hi Paul, how are things ?", user:"Bill", date:"26 June 2023 @ 15:45"},
+    {message:"Yeah Paul, how are you ? It's been a while !", user:"Owen", date:"26 June 2023 @ 18:12"},
+    {message:"Hey guys! Yeah doing good thanks and you guys ?", user:"Paul", date:this.messageDate}
+  ];
 }
